505eb1db298f5752fb12386706112f124269e789
Bozheng Liu <lb12016297@gmail.com>
2026-02-10 17:55:56 -0600

    Move token to .env file

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { REST, Routes } = require('discord.js');
+require("dotenv").config();
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -13,10 +14,9 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-// ⚠️ 需要你填的 3 个东西
-const CLIENT_ID = '1469059865849893116';
-const GUILD_ID = '1073767351465553961';
-const TOKEN = 'MTQ2OTA1OTg2NTg0OTg5MzExNg.Gb14c1.yhIHSMKmNouDJYqlRIH7IJG90n_8Kf2GaV4ItE';
+const CLIENT_ID_TOKEN = process.env.CLIENT_ID;
+const GUILD_ID_TOKEN = process.env.GUILD_ID;
+const TOKEN = process.env.DISCORD_TOKEN;
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
@@ -25,7 +25,7 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
     console.log('⏳ 正在注册 Slash Commands...');
 
     await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      Routes.applicationGuildCommands(CLIENT_ID_TOKEN, GUILD_ID_TOKEN),
       { body: commands },
     );
 
